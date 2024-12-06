ecc6b6dfca9a3c781cd4cb3f69999d4596536ea0
aki <aki@mymail.com>
2024-12-07 01:42:30 +0900

    std::move()を使ってcopyを行なわないようにする

---
+++ src_cpp/use_move.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <numeric>
+
+auto sum(const std::vector<double>& v) -> double {
+    return std::accumulate(v.begin(), v.end(), 0, std::plus<double>());
+}
+
+auto mean(const std::vector<double>& v) -> double {
+    return sum(v) / static_cast<double>(v.size());
+}
+
+// vをcopyせずにmoveする
+auto squared(std::vector<double>&& v) -> std::vector<double> {
+    std::transform(v.begin(), v.end(), v.begin(), [](double d) {return d * d;});
+    return std::move(v);
+}
+
+// rmsを計算する
+auto rms(std::vector<double>&& v) -> double {
+    return std::sqrt(mean(squared(std::move(v))));
+}
+
+auto main(void) -> int {
+    auto res = rms(std::vector<double>{1,2,3});
+    std::cout << "res = " << res << "\n";
+
+    return 0;
+}
